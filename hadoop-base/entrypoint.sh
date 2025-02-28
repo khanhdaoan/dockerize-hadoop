13a3ff0426480c18d97da8357bd7a550e30c1d18
khanhdaoan <khanh.daoan@gmail.com>
2025-02-28 22:58:09 +0700

    hadoop base

--- hadoop-base/entrypoint.sh
+++ hadoop-base/entrypoint.sh
@@ -1,49 +1,6 @@
 #!/bin/bash
 
-# Function: mvb
-# A wrapper for mv that backup instead of overwriting file
-# Usage: mvb source destination
-mvb() {
-	# Check for 2 arguments
-	if [ "$#" -ne 2 ]; then
-		echo "Usage: mvb source destination"
-		return 1
-	fi
-	
-	local src="$1"
-	local dst="$2"
-	
-	# Check if source exists
-	if [ ! -e "$src" ]; then
-		echo "Error: '$src' does not exist"
-		return 1
-	fi
-
-	# Setup local var
-	local src_basename=$(basename "$src")
-	local backup="${dst}.bak"
-
-	# If destination is a directory, append filename
-	if [ -d "$dst" ]; then
-		dst="${dst%/}/${src_basename}"
-	fi
-	
-	# Check if destination file exists
-	if [ -f "$dst" ]; then
-		if [ -e "$backup" ]; then
-			# If backup already exists, do nothing
-			echo "Backup already exists"
-		else
-			# Create backup
-			cp -p "$dst" "$backup"
-			echo "Backed up '$dst' to '$backup'"
-		fi
-	fi
-	
-	# Move
-	mv "$src" "$dst"
-	echo "Moved '$src' to '$dst'"
-}
+# Base entrypoint, create exclusion file
 
 # Initialization code
 if [ ! -f "/.initialized" ]; then
@@ -53,7 +10,7 @@ if [ ! -f "/.initialized" ]; then
 
 # Set initialization flag
   touch "/.initialized"
-  echo "Container initialized" >> /.initialized
+  echo "Hadoop base image initialized" >> /.initialized
 fi
 
 # Execute the command passed to docker run
